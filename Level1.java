4043fa6433619d0b5dbb0d5bf3d1321f5ae50b26
Rocket47 <romtswork@gmail.com>
2019-05-08 21:12:01 +0300

    Initial version of task1

--- Level1.java
+++ Level1.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
+import java.util.*;
+
 public class Level1
 {
     public static int squirrel(int N)
     {
-        return 1;
+        int valueFactorial = 1;
+        for (int i = 1; i  <= N; i++) {
+            valueFactorial = valueFactorial * i;
+        }
+        while (valueFactorial >= 10) {
+            valueFactorial /= 10;
+        }
+        return valueFactorial;
     }
 }
