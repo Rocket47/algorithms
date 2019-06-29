322a6f64c71f9e6dcef7831675dc97edf0cfdc9d
Rocket47 <romtswork@gmail.com>
2019-06-29 14:47:39 +0300

    Last version of task3

--- Level1.cs
+++ Level1.cs
@@ -1,115 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Level1Space
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(getDay(3, 4, 2, new int[] { 2, 2, 1, 3 }));
-            Console.ReadKey();
-        }
-
+    public static class Level1
+    {           
         public static int getDay(int N, int M, int L, int[] battalion)
         {
             bool flag = true;
-            int count = 0;
+            int count = 1;
             int n = 0;
             int m = 0;
-            int[,] array = new int[N, M];
-
-            //Заполнение 1 высадившихся десантников
-            //**************************************
+            int[,] array = new int[N, M];                   
             for (int i = 0; i < battalion.Length; i++)
             {
                 if (i % 2 == 0)
                 {
-                    n = battalion[i];
+                    n = battalion[i] - 1;
                     continue;
                 }
                 if (i % 2 != 0)
                 {
-                    m = battalion[i];
+                    m = battalion[i] - 1;
                 }
                 array[n, m] = 1;
+            }                 
+            while (flag)
+            {
+                count++;                
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < M; k++)
+                    {
+                        if ((array[j, k]) != 0)
+                        {
+                            array[j, k]++;
+                        }
+                    }
+                }                               
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < M; k++)
+                    {
+                        int sosedN1 = j - 1;
+                        int sosedN2 = j + 1;
+                        int sosedM1 = k - 1;
+                        int sosedM2 = k + 1;
+                        if ((array[j, k]) >= 2 && sosedN1 >= 0)
+                        {
+                            array[sosedN1, k]++;
+                        }
+                        if ((array[j, k]) >= 2 && sosedN2 < N)
+                        {
+                            array[sosedN2, k]++;
+                        }
+                        if ((array[j, k]) >= 2 && sosedM1 >= 0)
+                        {
+                            array[j, sosedM1]++;
+                        }
+                        if ((array[j, k]) >= 2 && sosedM2 < M)
+                        {
+                            array[j, sosedM2]++;
+                        }
+                    }
+                }
+                flag = false;
+                for (int j = 0; j < N; j++)
+                {
+                    for (int p = 0; p < M; p++)
+                    {
+                        if (array[j, p] == 0)
+                        {
+                            flag = true;
+                        }
+                    }
+                }              
+                Console.WriteLine();
             }
-            //**************************************
-            //while (flag)
-            //{
-            //    count++;
-            //    //Заполнение массива +1 к высадившимся
-            //    for (int j = 0; j < N; j++)
-            //    {
-            //        for (int k = 0; k < M; k++)
-            //        {
-            //            if ((array[j, k]) != 0)
-            //            {
-            //                array[j, k]++;
-            //            }
-            //        }
-            //    }
-            //    //*************************************************
-
-            //    //Заполнение соседних клеток
-            //    for (int j = 0; j < N; j++)
-            //    {
-            //        for (int k = 0; k < M; k++)
-            //        {
-            //            int sosedN1 = j - 1;
-            //            int sosedN2 = j + 1;
-            //            int sosedM1 = k - 1;
-            //            int sosedM2 = k + 1;
-            //            if ((array[j, k]) >= 2 && sosedN1 >= 0)
-            //            {
-            //                array[sosedN1, k]++;
-            //            }
-            //            if ((array[j, k]) >= 2 && sosedN2 < N)
-            //            {
-            //                array[sosedN2, k]++;
-            //            }
-            //            if ((array[j, k]) >= 2 && sosedM1 >= 0)
-            //            {
-            //                array[j, sosedM1]++;
-            //            }
-            //            if ((array[j, k]) >= 2 && sosedM2 < M)
-            //            {
-            //                array[j, sosedM2]++;
-            //            }
-            //        }
-            //    }
-
-            //    //Вывод массива
-            //    //*********************************
-            //    for (int j = 0; j < N; j++)
-            //    {
-            //        for (int p = 0; p < M; p++)
-            //        {
-            //            Console.Write(array[j, p]);
-            //        }
-            //        Console.WriteLine();
-            //    }
-            //    //**********************************  
-            //    flag = false;
-
-            //    for (int j = 0; j < N; j++)
-            //    {
-            //        for (int p = 0; p < M; p++)
-            //        {
-            //            if (array[j, p] == 0)
-            //            {
-            //                flag = true;
-                            
-            //            }
-            //        }
-            //    }
-            //    Console.WriteLine();                
-            //}
             return count;
-        }
+        }                        
     }
 }
     
