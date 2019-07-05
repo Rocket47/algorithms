b4841386394b517b2acf50f5358a0572cc85c236
Rocket47 <romtswork@gmail.com>
2019-07-05 10:00:45 +0300

    initial version of task5

---
+++ Level05.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static string PatternUnlock(int N, int[] hits)
+        {
+            double counter = 0.0;
+            double hypotenuse = 1.41421;
+            Dictionary<int, double> sequenceDictionary = createScheme();
+            for (int i = 0; i < N - 1; i++)
+            {
+                if ((Math.Abs((sequenceDictionary[hits[i]] - sequenceDictionary[hits[i + 1]])) > 1.00001))
+                {
+                    counter = counter + hypotenuse;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            counter = counter * 100000;
+            return counter.ToString().Replace("0", "");
+        }
+
+        public static Dictionary<int, double> createScheme()
+        {
+            Dictionary<int, double> coordinates = new Dictionary<int, double>();
+            coordinates.Add(6, 0.0);
+            coordinates.Add(1, 0.1);
+            coordinates.Add(9, 0.2);
+            coordinates.Add(5, 1.0);
+            coordinates.Add(2, 1.1);
+            coordinates.Add(8, 1.2);
+            coordinates.Add(4, 2.0);
+            coordinates.Add(3, 2.1);
+            coordinates.Add(7, 2.2);
+            return coordinates;
+        }
+    }
+}
