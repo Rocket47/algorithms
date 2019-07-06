6fafe30126b92035a9c98d6a40320c693e785e45
Rocket47 <romtswork@gmail.com>
2019-07-06 16:03:54 +0300

    Initial version of task 6

---
+++ Level06.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            showArr();
+            Console.ReadKey();
+        }
+
+        public static int[] WordSearch(int len, string s, string subs)
+        {
+            string[] stringAfterSplit = s.Split(' ');
+            return null;
+        }
+
+        public static void showArr()
+        {
+            string s = "1) строка разбивается на набор строк через выравнивание по заданной ширине.";
+            string[] arr = s.Split(' ');
+            int lengthStringCounter = 0;
+            string resultString = "";
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                
+                char[] charArr = arr[i].ToCharArray();
+                for (int j = 0; j < charArr.Length; j++)
+                {
+                        resultString = resultString + charArr[j].ToString();
+                        lengthStringCounter++;
+                        if (lengthStringCounter > 12)
+                        {
+                            resultString = resultString + Environment.NewLine;
+                        }                       
+                }
+                if ((12 - lengthStringCounter) < arr[(i+1)].Length)
+                {
+                    resultString = resultString + Environment.NewLine;
+                    lengthStringCounter = 0;
+                } else
+                {
+                    resultString = resultString + " ";
+                }
+            }
+            Console.WriteLine(resultString);
+        }
+    }
+}
