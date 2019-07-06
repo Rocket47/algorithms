ee6050aa0156b13cf55c3ba6e70ffcc16a3df9b0
Rocket47 <romtswork@gmail.com>
2019-07-06 17:01:21 +0300

    Added logic for ouput array with 0 1 result

--- Level06.cs
+++ Level06.cs
@@ -10,50 +10,67 @@ namespace Level06
     {
         static void Main(string[] args)
         {
-            showArr();
+            string s = "1) строка разбиваетсяИТестируется на набор строк через выравнивание по заданной ширине.";
+            int[] testOutPutArray =  WordSearch(12, s, "через");
+            Console.WriteLine("[{0}]", string.Join(", ", testOutPutArray));
             Console.ReadKey();
         }
-
         public static int[] WordSearch(int len, string s, string subs)
         {
-            string[] stringAfterSplit = s.Split(' ');
-            return null;
-        }
-
-        public static void showArr()
-        {
-            string s = "1) строка разбиваетсяИТестируется на набор строк через выравнивание по заданной ширине.";
-            string[] arr = s.Split(' ');
+            string[] splitArr = s.Split(' ');
+            string[] resultArray;
             int lengthStringCounter = 0;
             string resultString = "";
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < splitArr.Length; i++)
             {
-
-                char[] charArr = arr[i].ToCharArray();
+                char[] charArr = splitArr[i].ToCharArray();
                 for (int j = 0; j < charArr.Length; j++)
                 {
                     resultString = resultString + charArr[j].ToString();
                     lengthStringCounter++;
-                    if (lengthStringCounter >= 12)
+                    if (lengthStringCounter >= len)
                     {
                         resultString = resultString + Environment.NewLine;
                         lengthStringCounter = 0;
                     }
                 }
-              
-                    if ((i < arr.Length - 1) && ((12 - lengthStringCounter) < arr[(i + 1)].Length))
-                    {
-                        resultString = resultString + Environment.NewLine;
-                        lengthStringCounter = 0;
-                    }
-                    else
-                    {
-                        resultString = resultString + " ";
-                        lengthStringCounter++;
-                    }
-                
+
+                if ((i < splitArr.Length - 1) && ((len - lengthStringCounter) < splitArr[(i + 1)].Length))
+                {
+                    resultString = resultString + Environment.NewLine;
+                    lengthStringCounter = 0;
+                }
+                else
+                {
+                    resultString = resultString + " ";
+                    lengthStringCounter++;
+                }
             }
-            Console.WriteLine(resultString);
+
+            resultArray = new[] { resultString };
+            foreach(string n in resultArray)
+            {
+                Console.WriteLine(n);
+            }
+            return checkWord(resultArray, subs);
+        }
+
+        public static int[] checkWord(string[] arrWithWords, string word)
+        {
+            int counter = 0;
+            int[] resultArray = new int[arrWithWords.Length]; 
+            foreach (string searchWord in arrWithWords)
+            {
+                if (searchWord.Equals(word))
+                {
+                    resultArray[counter] = 1;
+                }
+                else
+                {
+                    resultArray[counter] = 0;
+                }
+            }
+            return resultArray;
         }
     }
 }
