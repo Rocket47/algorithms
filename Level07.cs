2a1de853e98ce40be2c9cedbac78fd02a7710406
Rocket47 <romtswork@gmail.com>
2019-07-09 23:40:27 +0300

    Created task 7

---
+++ Level07.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+  public static class Level1
+  {
+      public static int SumOfThe( int N, int [] data )
+      {
+        return 0 ;
+      }
+  }
+}
