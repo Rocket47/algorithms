b43df109ab66ae9ca05fe337d9033e1d7d6bdc2d
Rocket47 <romtswork@gmail.com>
2019-07-10 23:42:15 +0300

    Added method of counting the sum of array elements

--- Level07.cs
+++ Level07.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 
 namespace Level1Space
 {
-  public static class Level1
-  {
-      public static int SumOfThe( int N, int [] data )
-      {
-        return 0 ;
-      }
-  }
+   public static class Level1
+    {
+        public static int SumOfThe(int N, int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] != getSumNumbersOfArray(data))
+                {
+                   for (int k = 0; k < data.Length; k++)
+                    {
+
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public static int getSumNumbersOfArray(int[] arrayWithoutFirstElement)
+        {            
+            int sum = 0;
+            for (int i = 1; i < arrayWithoutFirstElement.Length; i++)
+            {
+                sum = sum + arrayWithoutFirstElement[i];
+            }               
+            return sum;
+        }
+    }
 }
