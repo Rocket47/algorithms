e44f43675c42ef6789affa7d9b25e7614d508865
Rocket47 <romtswork@gmail.com>
2019-07-11 00:15:33 +0300

    The function returns the number that is the sum of all elements of the array

--- Level07.cs
+++ Level07.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 
 namespace Level1Space
 {
-   public static class Level1
+  public static class Level1
     {
         public static int SumOfThe(int N, int[] data)
         {
+            int resultValue = 0;
             for (int i = 0; i < data.Length; i++)
             {
-                if (data[i] != getSumNumbersOfArray(data))
+                if (data[0] != getSumNumbersOfArray(data))
                 {
-                   for (int k = 0; k < data.Length; k++)
+                    int saveValFirstElement = data[0];
+                    for (int k = 1; k < data.Length; k++)
                     {
-
+                        data[k - 1] = data[k];
                     }
+                    data[data.Length - 1] = saveValFirstElement;
+                }
+                else
+                {
+                    resultValue = data[0];
+                    break;
                 }
             }
-            return 0;
+            return resultValue;
         }
 
         public static int getSumNumbersOfArray(int[] arrayWithoutFirstElement)
-        {            
+        {
             int sum = 0;
             for (int i = 1; i < arrayWithoutFirstElement.Length; i++)
             {
                 sum = sum + arrayWithoutFirstElement[i];
-            }               
+            }
             return sum;
         }
     }
