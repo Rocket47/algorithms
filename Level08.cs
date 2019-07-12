54751cdd1693a0ee70539620bbcac35d50065db4
Rocket47 <romtswork@gmail.com>
2019-07-12 23:52:07 +0300

    Created task 8

---
+++ Level08.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+  public static class Level1
+  {
+      public static int HowManyTimes(string s, string s_generic)
+      {
+        return .0 ;
+      }
+  }
+}
