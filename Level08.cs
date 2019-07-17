ab9f64701eea3e0e111d849a1d89d6a8fe5a319d
Rocket47 <romtswork@gmail.com>
2019-07-18 01:20:06 +0300

    Created first step of logic

--- Level08.cs
+++ Level08.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 
 namespace Level1Space
 {
-  public static class Level1
-  {
-      public static int HowManyTimes(string s, string s_generic)
-      {
-        return .0 ;
-      }
-  }
+    public static class Level1
+    {
+       
+        static void Main(string[] args)
+        {
+            HowManyTimes("123", "1102353");
+            Console.ReadKey();
+        }
+
+        public static int HowManyTimes(string s, string s_generic)
+        {            
+            char[] inputStringToChar = s.ToCharArray();
+            char[] genericStringToChar = s_generic.ToCharArray();
+            int counter = 0;
+            Dictionary<int, List<int>> list = new Dictionary<int, List<int>>();            
+            foreach (char i in inputStringToChar)
+            {
+                List<int> test = new List<int>();
+                for (int j = 0; j < genericStringToChar.Length; j++)
+                {                    
+                    if (i.Equals(genericStringToChar[j]))
+                    {
+                        test.Add(j);
+                    }
+                }
+                list.Add(counter, test);
+                counter++;
+            }
+
+            foreach (KeyValuePair<int, List<int>> kvp in list)
+            {
+                //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, String.Join(", ", kvp.Value));
+            }
+
+
+
+            return 0;
+           
+        }
+    }
 }
