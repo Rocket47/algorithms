bfc5dbc601857f91959d5c22412619004e09fd24
Rocket47 <romtswork@gmail.com>
2019-08-08 17:28:27 +0300

    Added logic for function Decrypt

--- Level08.cs
+++ Level08.cs
@@ -9,6 +9,7 @@ namespace Level1Space
         static void Main(string[] args)
         {
             TheRabbitsFoot("отдай мою кроличью лапку", true);
+            Decrypt("омоюу толл дюиа акчп йрьк");
             Console.ReadKey();
         }
 
@@ -18,7 +19,8 @@ namespace Level1Space
             if (encode)
             {
                 result = Encrypt(s);
-            } else
+            }
+            else
             {
 
             }
@@ -39,19 +41,15 @@ namespace Level1Space
             Console.WriteLine("____________Process of ecryption was started___________");
             Console.WriteLine("Length of this line: " + lengthString);
             Console.WriteLine("после вычисления квадратного корня: " + squareRoot);
-
             int afterPoint = (int)(((squareRoot - (int)squareRoot) * multiplier) / 10);
             Console.WriteLine("Number after point. Count of lines: " + afterPoint);
             int beforePoint = (int)squareRoot;
             Console.WriteLine("NUmber before point. Count of Columne: " + beforePoint);
-
             while (afterPoint * beforePoint < lengthString)
             {
                 beforePoint++;
             }
-
             char[,] matrixArr = new char[beforePoint, afterPoint];
-
             str = str.Replace(" ", String.Empty);
             char[] charArr = str.ToCharArray();
             int a = 0;
@@ -63,9 +61,7 @@ namespace Level1Space
                     a++;
                 }
             }
-
             //output 2d array
-
             int rowLength = matrixArr.GetLength(0);
             int colLength = matrixArr.GetLength(1);
 
@@ -93,11 +89,68 @@ namespace Level1Space
             result = result.Replace("\0", "");
             Console.WriteLine("Result of encryption: " + result);
             Console.WriteLine("____________Process of encryption was finished____________");
+            Console.WriteLine("************Next operation************");
             return result;
         }
 
         public static string Decrypt(string str)
         {
+            int lengthString = 0;
+            double squareRoot = 0;
+            int multiplier = 100;
+            string[] afterSplitting = str.Split(' ');
+            foreach (string i in afterSplitting)
+            {
+                lengthString += i.Length;
+            }
+            squareRoot = Math.Round(Math.Sqrt(lengthString), 2);
+            Console.WriteLine("____________Process of decryption was started___________");
+            Console.WriteLine("Length of this line: " + lengthString);
+            Console.WriteLine("после вычисления квадратного корня: " + squareRoot);
+            int afterPoint = (int)(((squareRoot - (int)squareRoot) * multiplier) / 10);
+            Console.WriteLine("Number after point. Count of lines: " + afterPoint);
+            int beforePoint = (int)squareRoot;
+            Console.WriteLine("NUmber before point. Count of Columne: " + beforePoint);
+            while (afterPoint * beforePoint < lengthString)
+            {
+                beforePoint++;
+            }
+            char[,] matrixArr = new char[beforePoint, afterPoint];
+            //--------------create matrix------------
+            int counter = 0;
+            for (int k = 0; k < afterPoint; k++)
+            {
+                char[] createArr = afterSplitting[counter].ToCharArray();
+                int index = 0;
+                for (int m = 0; m < beforePoint; m++)
+                {
+                    if (index < createArr.Length)
+                    {
+                        matrixArr[m, k] = createArr[index];
+                        char test = createArr[index];
+                        index++;
+                    }
+                }
+                counter++;
+            //-------------matrix was created-----------
+            }            
+
+            //output 2d array
+            int rowLength = matrixArr.GetLength(0);
+            int colLength = matrixArr.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", matrixArr[i, j]));
+                }
+
+                Console.WriteLine();
+            }
+            //---------------------------------//
+
+
             return null;
         }
     }
