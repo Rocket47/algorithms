c3202246e591953dd89db59357e0409ada559771
Rocket47 <romtswork@gmail.com>
2019-08-13 15:03:11 +0300

    Fixed result values

--- Level010.cs
+++ Level010.cs
@@ -8,9 +8,9 @@ namespace Level1Space
         public static string BigMinus(string s1, string s2)
         {
             double result = 0.0;
-            double a1 = Convert.ToDouble(s1);
-            double a2 = Convert.ToDouble(s2);
-            result = Math.Abs(a1 - a2);
+            double a1 = Convert.ToDouble(s1) / 10000000000000000;
+            double a2 = Convert.ToDouble(s2) / 10000000000000000;
+            result = Math.Abs(a1 - a2) * 10000000000000000;
             return result.ToString();  
         }
     }
