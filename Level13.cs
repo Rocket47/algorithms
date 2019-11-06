b88d1757ef9c2f417ad0c7fadf42f37c1022a2ac
Rocket47 <romtswork@gmail.com>
2019-11-06 13:04:34 +0300

    Fix issue +-1 value

--- Level13.cs
+++ Level13.cs
@@ -5,20 +5,28 @@ namespace Level1Space
 {
     public static class Level1
     {        
-        public static int Unmanned(int L, int N, int[][] track)
+         public static int Unmanned(int L, int N, int[][] track)
         {
             double resultTime = 0;
             resultTime = track[0][0];
             for (int i = 0; i < track.Length; i++)
             {
-                double percent = (resultTime / (track[i][1] + track[i][2]) * 100);
-                if (percent >= 0 && percent <= (track[i][1] * 100 / (track[i][1] + track[i][2]))) 
+                double percent = (resultTime / (track[i][1] + track[i][2]));
+                if (percent < 0.99)
+                {
+                    percent = percent * 100;                
+                }
+                else
+                {
+                    percent = (percent - Math.Truncate(percent)) * 100;
+                }
+                if (percent >= 0 && percent <= (track[i][1] * 100 / (track[i][1] + track[i][2])))
                 {
                     resultTime = resultTime + (track[i][1] - (track[i][1] * percent / (track[i][1] * 100 / (track[i][1] + track[i][2]))));
                 }
                 if ((i + 1) < track.Length)
                 {
-                    resultTime = resultTime + (track[i+1][0] - track[i][0]);
+                    resultTime = resultTime + (track[i + 1][0] - track[i][0]);
                 }
                 else
                 {
