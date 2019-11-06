b97e3b7c44b70b32b28cb679d69d4a4f2091acf9
Rocket47 <romtswork@gmail.com>
2019-11-06 14:38:23 +0300

    Fix if first traffic light more that all road

--- Level13.cs
+++ Level13.cs
@@ -8,32 +8,45 @@ namespace Level1Space
          public static int Unmanned(int L, int N, int[][] track)
         {
             double resultTime = 0;
-            resultTime = track[0][0];
-            for (int i = 0; i < track.Length; i++)
+            if (track[0][0] < L)
             {
-                double percent = (resultTime / (track[i][1] + track[i][2]));
-                if (percent < 0.99)
+                resultTime = track[0][0];
+                for (int i = 0; i < track.Length; i++)
                 {
-                    percent = percent * 100;                
-                }
-                else
-                {
-                    percent = (percent - Math.Truncate(percent)) * 100;
-                }
-                if (percent >= 0 && percent <= (track[i][1] * 100 / (track[i][1] + track[i][2])))
-                {
-                    resultTime = resultTime + (track[i][1] - (track[i][1] * percent / (track[i][1] * 100 / (track[i][1] + track[i][2]))));
-                }
-                if ((i + 1) < track.Length)
-                {
-                    resultTime = resultTime + (track[i + 1][0] - track[i][0]);
-                }
-                else
-                {
-                    resultTime = resultTime + (L - track[i][0]);
-                }
 
+                    double percent = (resultTime / (track[i][1] + track[i][2]));
+                    if (percent < 0.99)
+                    {
+                        percent = percent * 100;
+                    }
+                    else
+                    {
+                        percent = (percent - Math.Truncate(percent)) * 100;
+                    }
+                    if (percent >= 0 && percent <= (track[i][1] * 100 / (track[i][1] + track[i][2])))
+                    {
+                        resultTime = resultTime + (track[i][1] - (track[i][1] * percent / (track[i][1] * 100 / (track[i][1] + track[i][2]))));
+                    }
+                    if ((i + 1) < track.Length && track[i + 1][0] < L)
+                    {
+                        resultTime = resultTime + (track[i + 1][0] - track[i][0]);
+                    }
+                    else
+                    {
+                        if (track[i][0] > L)
+                        {
+                            break;
+                        }
+                        resultTime = resultTime + (L - track[i][0]);
+                    }
+
+                }
             }
+            else
+            {
+                resultTime = L;
+            }
+
             resultTime = Math.Round(resultTime);
             return (int)resultTime;
         }
