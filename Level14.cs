21bd2f2818c3d223e30bacb0a191f9f5bc0a8c44
Rocket47 <roman.tsenov@gmail.com>
2019-11-08 01:22:45 +0300

    Fix some issues after testing

--- Level14.cs
+++ Level14.cs
@@ -4,15 +4,7 @@ using System.Collections.Generic;
 namespace Level1Space
 {
     public static class Level1
-    {
-
-        public static void Main(string[] args)
-        {
-            int result = MaximumDiscount(3, new int[] { 400, 350, 300, 250, 200, 150, 100 });
-            Console.WriteLine($"Maximum discount is {result}");
-            Console.ReadKey();
-        }
-
+    {      
         public static int MaximumDiscount(int N, int[] price)
         {
             int maximumDiscountThreeGood = 0;
@@ -32,59 +24,59 @@ namespace Level1Space
             }          
 
             if (price.Length > 3)
-            {
-                int j = 0;
-                int minValue = price[0];
-                int counter = 0;               
+            {                                                          
                 int counterGoods = price.Length / 3;
-                for (int i = 0; i < counterGoods; i++)
-                {
-                    int min = price[j];                                        
-                    for (int k = 0; k < 3; k++)
-                    {
-                        int tmp = k;
-                        if (min > price[tmp + j])
-                        {
-                            min = price[tmp + j];
-                        }                        
-                    }
-                    maximumDiscountThreeGood += min;
-                    j = i + 3;
-                }
-                                       
-                for (int m = 0; m < price.Length - 1; m++)
-                {
-                    for (int l = m + 1; l < price.Length - 1; l++)
-                    {
-                        if (price[m] > price[l + 1])
-                        {
-                            int saveValue = price[m];
-                            price[m] = price[l + 1];
-                            price[l + 1] = saveValue;
-                        }
-                    }
-                }
-                
-                foreach (int test in price)
-                {
-                    Console.Write(test + " ");
-                }
-               while (counter < counterGoods)
-                {
-                    maximumDiscountAfterDivision += price[counter];
-                    counter++;
-                }
-
-                if (maximumDiscountAfterDivision > maximumDiscountThreeGood)
-                {
-                    finalDiscount = maximumDiscountAfterDivision;
-                }
-                else
-                {
-                    finalDiscount = maximumDiscountThreeGood;
-                }
+                maximumDiscountThreeGood = getDiscountThreeGood(counterGoods, price);
+                maximumDiscountAfterDivision = getDiscountAfterDivision(counterGoods, price);
+                finalDiscount = maximumDiscountAfterDivision >= maximumDiscountThreeGood ?  maximumDiscountAfterDivision : maximumDiscountThreeGood;                
             }
             return finalDiscount;
+        }
+
+        public static int getDiscountThreeGood(int counterGoods, int[] price)
+        {
+            int j = 0;
+            int discountThreeGood = 0;
+            for (int i = 0; i < counterGoods; i++)
+            {
+                int min = price[j];
+                for (int k = 0; k < 3; k++)
+                {
+                    int tmp = k;
+                    if (min > price[tmp + j])
+                    {
+                        min = price[tmp + j];
+                    }
+                }
+                discountThreeGood += min;
+                j = j + 3;
+            }
+            return discountThreeGood;
+        }
+
+        public static int getDiscountAfterDivision(int counterGoods, int[] price)
+        {
+            int counter = 0;
+            int discountAfterDivision = 0;
+            for (int m = 0; m < price.Length - 1; m++)
+            {
+                for (int l = m + 1; l < price.Length - 1; l++)
+                {
+                    if (price[m] > price[l + 1])
+                    {
+                        int saveValue = price[m];
+                        price[m] = price[l + 1];
+                        price[l + 1] = saveValue;
+                    }
+                }
+            }
+
+            while (counter < counterGoods)
+            {
+                discountAfterDivision += price[counter];
+                counter++;
+            }
+            return discountAfterDivision;
         }
     }
 }
