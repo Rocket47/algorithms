7a214a1c478b800c391fcac71455f3aeb3c33ffa
Rocket47 <romtswork@gmail.com>
2019-11-14 23:47:45 +0300

    Added gloval variable and list

--- Level17.cs
+++ Level17.cs
@@ -5,58 +5,69 @@ namespace Level1Space
 {
     public static class Level1
     {
+        public static string currentString;
+        public static List<string> saveStrings = new List<string>();
+
         public static void Main(string[] args)
         {
+            BastShoe("1 Hello");
             Console.WriteLine(BastShoe("2 2"));
+            Console.WriteLine(BastShoe("3 22"));
             Console.ReadKey();
         }
 
         public static string BastShoe(string command)
         {
             int key = 0;
-            string stringAfterKey;
-            string result = "";
+            string stringAfterKey;            
             key = Convert.ToInt32(command.Substring(0, command.IndexOf(" ")));
-            stringAfterKey = command.Substring(command.IndexOf(" ")).TrimStart(' ');
+            stringAfterKey = command.Substring(command.IndexOf(" ")).TrimStart(' ');            
             switch (key)
             {
                 case 1:
-                   result += Add(stringAfterKey);
-                   break;
+                    currentString += Add(stringAfterKey);
+                    saveStrings.Add(currentString);
+                    break;
                 case 2:
-                    result = Delete(stringAfterKey);
+                    currentString = Delete(stringAfterKey);
+                    saveStrings.Add(currentString);
                     break;
                 case 3:
-                    result = GiveOut();
+                    currentString = GiveOut(stringAfterKey);                   
                     break;
                 case 4:
-                    result = Undo();
+                    currentString = Undo();
                     break;
                 case 5:
-                    result = Redo();
+                    currentString = Redo();
                     break;
             }
-            return result;
+            return currentString;
         }
 
         public static string Add(string mStringAfterKey)
         {
             string result = "";
-            result += mStringAfterKey;
+            result += mStringAfterKey;           
             return result;
         }
 
         public static string Delete(string mStringAfterKey)
-        {
-            string result = "hello";
+        {            
             int countElementsForDeleting = Convert.ToInt32(mStringAfterKey);
-            result = result.Remove(result.Length - 2, countElementsForDeleting);
-            return result;
+            currentString = currentString.Remove(currentString.Length - 2, countElementsForDeleting);
+            return currentString;
         }
 
-        public static string GiveOut()
+        public static string GiveOut(string mIndex)
         {
-            return null;
+            string result = "";
+            int index = Convert.ToInt32(mIndex);
+            if (index < mIndex.Length)
+            {
+                result = currentString[index].ToString();
+            }            
+            return result;
         }
 
         public static string Undo()
