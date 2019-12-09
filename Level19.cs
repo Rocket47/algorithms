2fa55a890766153ba079f94bb1e7caa6eaee1fca
Rocket47 <romtswork@gmail.com>
2019-12-09 23:42:08 +0300

    Created file of the script for task 19

---
+++ Level19.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+  public static class Level1
+  {
+      public static boolean SherlockValidString(string s)
+      {
+        return true;
+      }
+  }
+}
