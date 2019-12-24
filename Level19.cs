4c0c552fa271b000b9f1a7f34f1e6a1f0a23aba7
Rocket47 <romtswork@gmail.com>
2019-12-24 20:27:06 +0300

    Created method to get count of each symbols in the input string

--- Level19.cs
+++ Level19.cs
@@ -1,13 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Level1Space
 {
-  public static class Level1
-  {
-      public static boolean SherlockValidString(string s)
-      {
-        return true;
-      }
-  }
+    public static class Level1
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool SherlockValidString(string s)
+        {
+            bool isValid = false;
+            Dictionary<int, char> countOfEachSymbolList = createDictionaryCountOfEachSymbols(s);
+
+            return isValid;
+        }
+
+        public static Dictionary<int, char> createDictionaryCountOfEachSymbols(string s)
+        {            
+            char[] arr = s.ToCharArray();
+            Dictionary<int, char> listHasCountOfEachSymbols = new Dictionary<int, char>();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int counter = 0;
+                if (listHasCountOfEachSymbols.ContainsValue(arr[i]))
+                {
+                    continue;
+                }
+                for (int j = 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        counter++;
+                    }
+                }
+                if (!listHasCountOfEachSymbols.ContainsValue(arr[i]))
+                {
+                    listHasCountOfEachSymbols.Add(counter, arr[i]);
+                }
+            }
+            return listHasCountOfEachSymbols;
+        }
+    }
 }
+
