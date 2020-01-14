5024c0e3cbd7a4f155a3baf872e8a1c738a5b1a7
Rocket47 <romtswork@gmail.com>
2020-01-14 16:41:43 +0300

    Implemented getting rows from each circle. Next step to turn forward each new string

--- Level20.cs
+++ Level20.cs
@@ -19,31 +19,58 @@ namespace Level1Space
             string oldDownSide = "";
             string leftSide = "";
             string rightSide = "";
+            string strToSort = "";
             int countOfSquare = 0;         
             countOfSquare = M < N ? (M / 2) : (N / 2);
 
             for (int i = 0; i < countOfSquare; i++)
             {
-                oldUpSide = Matrix[i].Substring(1, Matrix[i].Length - 2);
-                oldDownSide = Matrix[(Matrix.Length - 1) - i].Substring(1, Matrix[i].Length - 2);
-
                 for (int m = 0; m < Matrix.Length; m++)
                 {
                     leftSide += Matrix[m][i].ToString();
-                    rightSide += Matrix[m][Matrix.Length - i].ToString();
+                    rightSide += Matrix[m][Matrix[m].Length - i - 1].ToString();
                 }
-                leftSide = leftSide.Substring(1, leftSide.Length - 2);
-                rightSide = rightSide.Substring(1, rightSide.Length - 2);
+                if (i > 0)
+                {
+                    oldUpSide = Matrix[i].Substring(1, Matrix[i].Length - 2);
+                    oldDownSide = Matrix[(Matrix.Length - 1) - i].Substring(1, Matrix[i].Length - 2);
+                    leftSide = leftSide.Substring(1, leftSide.Length - 2);
+                    rightSide = rightSide.Substring(1, rightSide.Length - 2);
+                }
+                else
+                {
+                    oldUpSide = Matrix[i];
+                    oldDownSide = Matrix[(Matrix.Length - 1) - i];                                        
+                }
+                if (leftSide.Length > 2 && rightSide.Length > 2)
+                {
+                    char[] reverseArr = oldDownSide.ToCharArray();
+                    Array.Reverse(reverseArr);
+                    oldDownSide = new string(reverseArr);
+                    reverseArr = leftSide.ToCharArray();
+                    Array.Reverse(reverseArr);
+                    leftSide = new string(reverseArr);
+                    strToSort = oldUpSide + rightSide.Substring(1, rightSide.Length - 2) + oldDownSide + leftSide.Substring(1, leftSide.Length - 2);
+                }
+                else
+                {                   
+                    char[] reverseArr = oldDownSide.ToCharArray();
+                    Array.Reverse(reverseArr);
+                    oldDownSide = new string(reverseArr);
+                    strToSort = oldUpSide + oldDownSide;
+                }
                 Console.WriteLine(oldUpSide);
-                Console.WriteLine(leftSide);
                 Console.WriteLine(rightSide);
                 Console.WriteLine(oldDownSide);
+                Console.WriteLine(leftSide);
+                Console.WriteLine("Получилась строка: " + strToSort);
                 Console.WriteLine("********new cicle*********");
                 oldDownSide = "";
                 oldUpSide = "";
                 leftSide = "";
                 rightSide = "";
-            }
+                strToSort = "";
+            }                
         }
     }
 }
