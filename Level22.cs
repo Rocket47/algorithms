1dd0df47b1cbb59b8abd3125e976310a120c1726
Rocket47 <romtswork@gmail.com>
2020-01-27 23:51:50 +0300

    The result was added as a function parametr

--- Level22.cs
+++ Level22.cs
@@ -6,6 +6,7 @@ namespace Level1Space
 {
     public static class Level1
     {
+        public static List<string> resultList = new List<string>();
         public static void Main(string[] args)
         {
             Console.WriteLine(BalancedParentheses(3));
@@ -13,36 +14,42 @@ namespace Level1Space
         }
         public static string BalancedParentheses(int N)
         {
-            int SaveValue = N;
-            
+            string result = "";
+            DoRecursion(0, 0, N, result);
+            foreach (string n in resultList)
+            {
+                Console.Write(n);
+                Console.Write(" ");
+            }
+
             return result;
         }
 
-        public static string DoRecursion(int countOpenBracket, int countCloseBracket, int maxCountOpenBracket)
+        public static string DoRecursion(int countOpenBracket, int countCloseBracket, int maxCountOpenBracket, string result)
         {
-            string result = "";
             if (countCloseBracket < maxCountOpenBracket)
             {
                 if (countOpenBracket > countCloseBracket)
                 {
                     result += ")";
-                    return DoRecursion(countOpenBracket - 1, countCloseBracket, maxCountOpenBracket);
+                    countCloseBracket++;
+                    return DoRecursion(countOpenBracket, countCloseBracket, maxCountOpenBracket, result);
                 }
                 if (countOpenBracket < countCloseBracket)
                 {
                     result += "(";
-                    return DoRecursion(countOpenBracket, countCloseBracket - 1, maxCountOpenBracket);
+                    countOpenBracket++;
+                    return DoRecursion(countOpenBracket++, countCloseBracket++, maxCountOpenBracket, result);
                 }
                 if (countOpenBracket == countCloseBracket)
                 {
-
+                    resultList.Add(result);
+                    countOpenBracket++;
+                    countOpenBracket++;
                 }
-                return DoRecursion(countOpenBracket, countCloseBracket, maxCountOpenBracket);
+                return DoRecursion(countOpenBracket, countCloseBracket, maxCountOpenBracket, result);
             }
-           
+            return result;
         }
-
-
-       
     }
 }
