8d572f72805e2e90dac2d70a395cb6bffa0fdb6f
Rocket47 <romtswork@gmail.com>
2020-01-29 23:11:58 +0300

    Just try to some ideas

--- Level22.cs
+++ Level22.cs
@@ -10,22 +10,22 @@ namespace Level1Space
         public static void Main(string[] args)
         {
             BalancedParentheses(2);
-            Console.ReadKey();
-        }
-        public static string BalancedParentheses(int N)
-        {
-            string result = "";           
-            if (N > 0)
-            {
-                result = DoRecursion(0, 0, N, result);
-                N = N - 1;
-            }
             foreach (string n in resultList)
             {
                 Console.Write(n);
                 Console.Write(" ");
             }
-            return result;
+            Console.ReadKey();
+        }
+        public static string BalancedParentheses(int N)
+        {
+            string result = "";
+            if (N > 0)
+            {                
+                N = N - 1;
+                return DoRecursion(0, 0, N, result);              
+            }            
+            return BalancedParentheses(N);
         }
 
         public static string DoRecursion(int countOpenBracket, int countCloseBracket, int maxCountOpenBracket, string result)
@@ -43,11 +43,11 @@ namespace Level1Space
                     result += "(";
                     countOpenBracket++;
                     return DoRecursion(countOpenBracket, countCloseBracket, maxCountOpenBracket, result);
-                }
+                }                
             }
             if (countOpenBracket == countCloseBracket)
             {
-                resultList.Add(result);
+                resultList.Add(result);                                
             }
             return result;
         }
