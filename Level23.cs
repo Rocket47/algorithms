a58b829f21fac67e35e61759212e9161a4d58540
Rocket47 <romtswork@gmail.com>
2020-01-31 18:29:17 +0300

    Initial verison of the task 23

---
+++ Level23.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        static char[] ResultArr;
+        static string result = "";       
+     
+        public static bool Footbal(int[] F, int N)
+        {
+            bool result = false;
+            return result;
+        }
+
+        public static bool Check1stMethod(int[] F)
+        {
+            bool result = false;
+            return result;
+        }
+
+        public static bool Check2ndMethod(int[] F)
+        {
+            bool result = false;
+            return result;
+        }
+    }
+}
+
