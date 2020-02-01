d4c99886d4d440cfbaec3e426614e7b5d5b809bd
Rocket47 <romtswork@gmail.com>
2020-02-01 23:00:55 +0300

    Next step to do this task.

--- Level23.cs
+++ Level23.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 namespace Level1Space
 {
     public static class Level1
-    {
-        static char[] ResultArr;
-        static string result = "";       
-     
+    {      
         public static bool Footbal(int[] F, int N)
         {
             bool result = false;
@@ -16,7 +13,29 @@ namespace Level1Space
 
         public static bool Check1stMethod(int[] F)
         {
-            bool result = false;
+            bool result = true;
+            int previous = 1;
+            int saveErrorIndex = 0;
+            int saveNextAfterSmallerValue = 0;
+            int saveLast = 0;
+            int count = 0;
+            for (int i = 0; i < F.Length; i++)
+            {
+                if (F[i] < previous && count < 2)
+                {
+                    count = 1;
+                    saveNextAfterSmallerValue = i + 1;
+                    saveErrorIndex = i;
+                }
+                else if (count == 1 && F[i] > previous)
+                {
+                    saveLast = F[i + 1];
+                    if (F[i] > saveNextAfterSmallerValue)
+
+
+                }
+                previous = i;
+            }
             return result;
         }
 
