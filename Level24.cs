0b3c3360a42f0cf8a6b7955caf3a39c6d8c056e7
Rocket47 <romtswork@gmail.com>
2020-02-11 16:51:01 +0300

    1st logic of the task

--- Level24.cs
+++ Level24.cs
@@ -7,12 +7,29 @@ namespace Level1Space
     {
         public static void Main(string[] args)
         {
+            Keymaker(5);
+            Console.ReadKey();
         }
         public static string Keymaker(int k)
         {
             string result = "";
+            Dictionary<int, bool> ListDoors = new Dictionary<int, bool>();
+            for (int i = 1; i <= k; i++)
+            {
+                ListDoors.Add(i, false);
+            }
+            Console.WriteLine("Intiialize program...");            
+            ShowDictionary(ListDoors);
             return result;
         }
+
+        public static void ShowDictionary(Dictionary<int, bool> Dictionary)
+        {
+            foreach (KeyValuePair<int, bool> kvp in Dictionary)
+            {               
+                Console.WriteLine("Door = {0}, Status = {1}", kvp.Key, kvp.Value);
+            }
+        } 
     }
 }
     
