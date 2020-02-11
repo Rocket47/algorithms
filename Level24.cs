e1980899ab44bd77477453495824d0855c053b18
Rocket47 <romtswork@gmail.com>
2020-02-11 16:21:49 +0300

    Initial version of the task 24

---
+++ Level24.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public static string Keymaker(int k)
+        {
+            string result = "";
+            return result;
+        }
+    }
+}
+    
