915e448968dc33948b6f2663e6f43d800414f02d
Rocket47 <romtswork@gmail.com>
2020-02-13 10:57:38 +0300

    Created a separate folder and added initial version of the LinkedList

---
+++ AlgorithmsDataStructures/LinkedList.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+    public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int _value) { value = _value; }
+    }
+
+    public class LinkedList
+    {
+        public Node head;
+        public Node tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddInTail(Node _item)
+        {
+            if (head == null) head = _item;
+            else tail.next = _item;
+            tail = _item;
+        }
+
+        public Node Find(int _value)
+        {
+            Node node = head;
+            while (node != null)
+            {
+                if (node.value == _value) return node;
+                node = node.next;
+            }
+            return null;
+        }
+
+        public List<Node> FindAll(int _value)
+        {
+            List<Node> nodes = new List<Node>();
+            return nodes;
+        }
+
+        public bool Remove(int _value)
+        {
+            return true;
+        }
+
+        public void RemoveAll(int _value)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+
+        public void InsertAfter(Node _nodeAfter, Node _nodeToInsert)
+        {
+
+        }
+    }
+}
