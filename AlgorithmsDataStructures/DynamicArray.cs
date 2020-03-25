4ebebe00eecc0b32eed5261aa31ad9a18061f193
Rocket47 <romtswork@gmail.com>
2020-03-25 23:17:44 +0300

    Create template to DynamicArray

---
+++ AlgorithmsDataStructures/DynamicArray.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+   public class DynArray<T>
+   {
+     public T [] array;
+     public int count;
+     public int capacity;
+
+     public DynArray()
+     {
+       count = 0;
+       MakeArray(16);
+     }
+
+     public void MakeArray(int new_capacity)
+     {
+      
+     }
+
+     public T GetItem(int index)
+     {       
+       return default(T);
+     }
+
+     public void Append(T itm)
+     {
+       
+     }
+
+     public void Insert(T itm, int index)
+     {
+      
+     }
+
+     public void Remove(int index)
+     {
+       
+     }
+
+    }
+}
