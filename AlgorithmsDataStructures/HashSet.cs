3460d8543f70bb8eb9c94895cf3df7cc5e794b18
Rocket47 <romtswork@gmail.com>
2020-05-11 22:43:16 +0300

    Method SeekSlot() was added

--- AlgorithmsDataStructures/HashSet.cs
+++ AlgorithmsDataStructures/HashSet.cs
@@ -16,6 +16,7 @@ namespace AlgorithmsDataStructures
             for (int i = 0; i < 20000; i++) slots[i] = default(T);
         }
 
+        //*////////////////////////////////////////////////////////
         public int Size()
         {
             int count = 0;
@@ -26,11 +27,13 @@ namespace AlgorithmsDataStructures
             return count;
         }
 
+        //*////////////////////////////////////////////////////////
         public void Put(T value)
         {
             // всегда срабатывает
         }
 
+        //*////////////////////////////////////////////////////////
         public bool Get(T value)
         {
             // возвращает true если value имеется в множестве,
@@ -38,6 +41,7 @@ namespace AlgorithmsDataStructures
             return false;
         }
 
+        //*////////////////////////////////////////////////////////
         public bool Remove(T value)
         {
             // возвращает true если value удалено
@@ -45,24 +49,28 @@ namespace AlgorithmsDataStructures
             return false;
         }
 
+        //*////////////////////////////////////////////////////////
         public PowerSet<T> Intersection(PowerSet<T> set2)
         {
             // пересечение текущего множества и set2
             return null;
         }
 
+        //*////////////////////////////////////////////////////////
         public PowerSet<T> Union(PowerSet<T> set2)
         {
             // объединение текущего множества и set2
             return null;
         }
 
+        //*////////////////////////////////////////////////////////
         public PowerSet<T> Difference(PowerSet<T> set2)
         {
             // разница текущего множества и set2
             return null;
         }
 
+        //*////////////////////////////////////////////////////////
         public bool IsSubset(PowerSet<T> set2)
         {
             // возвращает true, если set2 есть
@@ -71,10 +79,12 @@ namespace AlgorithmsDataStructures
             return false;
         }
 
-        public int HashFun(string value)
+        //*////////////////////////////////////////////////////////
+        public int HashFun(T value)
         {
             int hashCode2 = 0;
-            char[] converterArray = value.ToCharArray();
+            string convertToStringValue = value.ToString();
+            char[] converterArray = convertToStringValue.ToCharArray();
             for (int i = 0; i < converterArray.Length; i++)
             {
                 byte[] bytesArray = BitConverter.GetBytes(converterArray[i]);
@@ -85,6 +95,35 @@ namespace AlgorithmsDataStructures
             }
             hashCode2 = hashCode2 % size;
             return hashCode2;
+        }
+
+        //*////////////////////////////////////////////////////////
+        public int SeekSlot(T value)
+        {
+            int index = HashFun(value);
+            int stepSize = 2;
+
+            if (index >= size)
+            {
+                return -1;
+            }
+            while (stepSize != 0)
+            {
+                for (int i = index; i < size; i++)
+                {
+                    if (slots[i] == null)
+                    {
+                        return i;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                index = 0;
+                stepSize--;
+            }
+            return -1;
         }
     }
 }
