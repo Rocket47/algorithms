9fcaac566d6153152e6f7fffe5c62cf90f4d9887
Rocket47 <romtswork@gmail.com>
2020-05-14 13:34:10 +0300

    Work version 2.0.0

--- AlgorithmsDataStructures/BloomFilter.cs
+++ AlgorithmsDataStructures/BloomFilter.cs
@@ -1,18 +1,20 @@
 using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Collections;
 
 namespace AlgorithmsDataStructures
 {
     public class BloomFilter
     {
         public int filter_len;
-        public byte[] filterArray;
+        public byte[] bytesFilter;       
+        
 
         public BloomFilter(int f_len)
         {
             filter_len = f_len;
-            filterArray = new byte[filter_len];
+            bytesFilter = new byte[filter_len / 8];            
         }
        
         //*//////////////////////////////////////////////////
@@ -23,7 +25,7 @@ namespace AlgorithmsDataStructures
             for (int i = 0; i < str1.Length; i++)
             {
                 int code = (int)str1[i];
-                result = (result * (17 + code)) % filter_len;
+                result = ((result + code) * 17 ) % filter_len;
             }
             
             return result;
@@ -36,7 +38,7 @@ namespace AlgorithmsDataStructures
             for (int i = 0; i < str1.Length; i++) 
             {
                 int code = (int)str1[i];
-                result = (result * (223 + code)) % filter_len;
+                result = ((result + code) * 223) % filter_len;
             }
             return result;
         }
@@ -46,31 +48,22 @@ namespace AlgorithmsDataStructures
         {
             int position1 = Hash1(str1);
             int position2 = Hash2(str1);
-            filterArray[position1] = 1;
-            filterArray[position2] = 1;                       
+            int hash1 = position1 & 0x7FFFFFFF; 
+            int hash2 = position2 & 0x7FFFFFFF;
+            byte bit1 = (byte)(1 << (hash1 & 7)); 
+            byte bit2 = (byte)(1 << (hash2 & 7)); 
+            bytesFilter[hash1 % bytesFilter.Length] |= bit1;
+            bytesFilter[hash2 % bytesFilter.Length] |= bit2;
         }
 
         //*//////////////////////////////////////////////////
         public bool IsValue(string str1)
         {
-            int position1 = Hash1(str1);
-            int position2 = Hash2(str1);
-            if (filterArray[position1] == 0 && filterArray[position2] == 0)
-            {
-                return false;
-            }
-            else if (filterArray[position1] == 0 && filterArray[position2] == 1)
-            {
-                return false;
-            }
-            else if (filterArray[position1] == 1 && filterArray[position2] == 0)
-            {
-                return false;
-            } 
-            else
-            {
-                return true;
-            }          
+            int hash1 = Hash1(str1) & 0x7FFFFFFF;
+            int hash2 = Hash2(str1) & 0x7FFFFFFF;
+            byte bit1 = (byte)(1 << (hash1 & 7)); 
+            byte bit2 = (byte)(1 << (hash2 & 7)); 
+            return (bytesFilter[hash1 % bytesFilter.Length] & bit1) != 0 && (bytesFilter[hash2 % bytesFilter.Length] & bit2) != 0;
         }
     }
 }
