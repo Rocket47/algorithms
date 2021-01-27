e9311d9ff07f8463a4079daa30c67c8f7d3ee430
Rocket47 <romtswork@gmail.com>
2021-01-27 23:22:24 +0300

    version 2.0. Fix issues

--- AlgorithmsDataStructures/Heap.cs
+++ AlgorithmsDataStructures/Heap.cs
@@ -5,32 +5,27 @@ namespace AlgorithmsDataStructures2
 {
 	public class Heap
 	{
-
-		public int[] HeapArray; // хранит неотрицательные числа-ключи
-		int countInsert = 0;
+		public int[] HeapArray; // хранит неотрицательные числа-ключи		
 
 		public Heap() { HeapArray = null; }
 
 		public void MakeHeap(int[] a, int depth)
-		{
-			if (a == null) { return; }
-
-			if (depth < 0) { return; }
-
+		{			
 			int heapArraySize;
-			if (depth == 0)
+
+			int power = 0;
+			if (depth >= 0)
 			{
-				heapArraySize = 1;
+				power = depth + 1;
 			}
-			else
-			{
-				var power = depth + 1;
-				heapArraySize = (int)Math.Pow(2, power) - 1;
-			}
+		    heapArraySize = (int)Math.Pow(2, power) - 1;		
 			HeapArray = new int[heapArraySize];
-			for (int i = 0; i < a.Length; i++)
+			if (a != null)
 			{
-				Add(a[i]);
+				for (int i = 0; i < a.Length; i++)
+				{
+					Add(a[i]);
+				}
 			}
 		}
 
@@ -88,52 +83,45 @@ namespace AlgorithmsDataStructures2
 
 		public bool Add(int key)
 		{
-			if (key < 0 || HeapArray == null) { return false; }
-
-			var count = HeapArray.Length - 1;
-
-			if (count == 0)
+			if (HeapArray[0] == 0)
 			{
 				HeapArray[0] = key;
 				return true;
 			}
 
-			if (count >= 1)
+			int index = GetIndexNull(HeapArray);
+			if (index != -1)
 			{
-				if (countInsert < count)
-				{
-					HeapArray[countInsert] = key;
-					countInsert++;
-					HeapArray = ShuffleAdd(HeapArray);
-					return true;
-				}
+				HeapArray[index] = key;
+				ShuffleAdd(index);
+				return true;
 			}
-			return false; // если куча вся заполнена
+			else
+			{
+				return false;
+			}
 		}
 
-		public int[] ShuffleAdd(int[] array)
+		public void ShuffleAdd(int index)
 		{
-			if (array != null)
+			int temp;
+			while (HeapArray[index] > HeapArray[(index - 1) / 2])
 			{
-				var lastPosition = GetIndexLastElement(array);
-				while (true)
-				{
-					var parent = (int)Math.Floor((double)(lastPosition - 1) / 2);
-					if (parent < 0) { break; }
-					if (array[lastPosition] > array[parent])
-					{
-						int tmp = array[parent];
-						array[parent] = array[lastPosition];
-						array[lastPosition] = tmp;
-						lastPosition = parent;
-					}
-					else
-					{
-						break;
-					}
-				}
+				temp = HeapArray[(index - 1) / 2];
+				HeapArray[(index - 1) / 2] = HeapArray[index];
+				HeapArray[index] = temp;
+				index = (index - 1) / 2;
 			}
-			return array;
+		}
+
+		public int GetIndexNull(int[] a)
+		{
+			for (int i = 0; i < a.Length; i++)
+			{
+				if (a[i] == 0) return i;
+
+			}
+			return -1;
 		}
 
 		public int GetIndexLastElement(int[] array)
