45a3cba20f741e74a651a6e4558cc10e1605987a
Rocket47 <romtswork@gmail.com>
2021-12-12 00:16:23 +0300

    Fix lesson 4 according to the template

--- LearnF#/Lesson4.fs
+++ LearnF#/Lesson4.fs
@@ -1,18 +1,14 @@
 open System
 // 7.1.1
-let rec fibo n =
-    match n with
-    | 0 -> n 
-    | 1 -> n
-    | _ -> fibo (n-1) + fibo (n-2)
-
+let rec fibo = function
+ | 0 -> 0
+ | 1  -> 1
+ | n  -> fibo(n - 2) + fibo(n - 1)
+ 
 // 7.1.2
-let rec sum n = 
-    match n with
-    | 1 -> 1
-    | _ -> n + sum(n - 1)
-    
-printfn "%d" (sum 5)
+let rec sum = function
+ | 1  -> 1
+ | n  -> n + sum(n - 1)
 
 // 7.1.3
 let rec sum2 = function 
